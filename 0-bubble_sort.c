c5f3f4af47cf33b7265afd167cea8e66eb77f530
MarB-T <danmarbet@gmail.com>
2023-06-25 13:52:56 +0300

    bubble sort correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	bool swapped;
 
+	if (array == NULL)
+		return;
+	if (size <= 0)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = false;
