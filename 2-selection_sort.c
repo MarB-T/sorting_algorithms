dc13d16861b1b736eb2d9b8c1a15f85e29265769
MarB-T <danmarbet@gmail.com>
2023-06-26 23:42:45 +0300

    selection sort of an integer array update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,7 +6,31 @@
  * @size: size of array
  * Return: nothing
  */
-void selection_sort(int *array. size_t size)
+void selection_sort(int *array, size_t size)
 {
+	int temp, min;
+	size_t i, j, t;
 
+	if (array == NULL || size <= 0)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		t = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < min)
+			{
+				min = array[j];
+				t = j;
+			}
+		}
+		if (min != array[i])
+		{
+			temp = array[i];
+			array[i] = min;
+			array[t] = temp;
+			print_array(array, size);
+		}
+	}
 }
