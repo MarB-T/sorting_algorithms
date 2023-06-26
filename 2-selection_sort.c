fcd961158afdb9f6d7e07781bfc5a3396cc61681
MarB-T <danmarbet@gmail.com>
2023-06-26 22:18:29 +0300

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort an array of integers in ascending order using selection sort
+ * @array: array to sort
+ * @size: size of array
+ * Return: nothing
+ */
+void selection_sort(int *array. size_t size)
+{
+
+}
