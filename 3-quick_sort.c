1f74c50b925cfc282126372e55e936050e5032a6
MarB-T <danmarbet@gmail.com>
2023-06-27 23:43:15 +0300

    Quick sort update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,14 @@
 #include "sort.h"
 
 /**
- * swap - swaps integers
- * @a: pointer to first int
- * @b: pointer to second int
+ * swap - swaps two integers
+ * @a: first int
+ * @b: second int
  * Return: nothing
  */
-void swap(int *a, int *b)
+void swap(int* a, int* b)
 {
 	int temp = *a;
-
 	*a = *b;
 	*b = temp;
 }
@@ -17,41 +16,69 @@ void swap(int *a, int *b)
 /**
  * partition - partition an array
  * @array: array to partition
+ * @low: the lowest index of array
+ * @high: the highest index of array
  * @size: size of array
- * Return: nothing
+ * Return: pivot index for quick sorting
  */
-int partition(int *array, size_t size)
+int partition(int *array, int low, int high, size_t size)
 {
-	size_t i = 0, j = 0;
-	int pivot = array[size - 1];
+	int pivot, i, j;
 
-	for (j = 0; j < size - 1; j++)
+	i = low - 1;
+	j = low;
+	pivot = array[high];
+	while (j < high)
 	{
 		if (array[j] <= pivot)
 		{
-			swap(&array[j], &array[i]);
 			i++;
+			if (i != j)
+			{
+				swap(&array[j], &array[i]);
+				print_array(array, size);
+			}
 		}
+		j++;
 	}
-	swap(&array[i], &array[size - 1]);
-	return (i);
+	if (pivot < array[i + 1])
+		swap(&array[i + 1], &array[high]);
+	return (1 + i);
 }
 
 /**
- * quick_sort - sort an array of ints using quick_sort
+ * sort - sort an array of ints using quick_sort
  * @array: the array to be sorted
+ * @low: low index of array section
+ * @high: last index of array section
  * @size: size of the array
+ * Return: nothing
+ */
+void sort(int *array, int low, int high, size_t size)
+{
+	int pivot_indx;
+
+	if (low < high)
+	{
+		pivot_indx = partition(array, low, high, size);
+		sort(array, low, pivot_indx -1, size);
+		sort(array, pivot_indx + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - sort an array of using qick sort algorithm
+ * @array: the array to sort
+ * @size: the size of array
  * Return: nothing
  */
 void quick_sort(int *array, size_t size)
 {
-	int pivot_indx;
+	size_t low, high;
 
-	if (array == NULL || size <= 0)
+	if (size == 0 || size == 1)
 		return;
-	if (size < 2)
-		return;
-	pivot_indx = partition(array, size);
-	quick_sort(array, pivot_indx);
-	quick_sort(array + pivot_indx + 1, size - pivot_indx - 1);
+	low = 0;
+	high = size - 1;
+	sort(array, low, high, size);
 }
